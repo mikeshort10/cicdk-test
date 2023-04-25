d4a648e068a58b7d8414eb7e5bf04e14220175da
Mike Short <mikeshort2014@gmail.com>
2023-04-25 11:43:21 -0400

    Initial commit

---
+++ test/test-cicd-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestCicdCdk from '../lib/test-cicd-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test-cicd-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestCicdCdk.TestCicdCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
