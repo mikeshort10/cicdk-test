637df12576c2d5f0cb3937701526fdfd94717cb0
Mike Short <mikeshort2014@gmail.com>
2023-04-26 14:31:16 -0400

    feat: add env specific deploys

--- bin/test-cicd-cdk.ts
+++ bin/test-cicd-cdk.ts
@@ -1,21 +1,68 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { TestCicdCdkStack } from '../lib/test-cicd-cdk-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import {
+  StackConfig,
+  TagResource,
+  TestCicdCdkStack,
+} from "../lib/test-cicd-cdk-stack";
+
+type Environment = "devel" | "alpha" | "prod";
+
+const $namespace = (repo: string, env: Environment) => {
+  return function namespace(baseName: string) {
+    return [repo, baseName, env].join("-");
+  };
+};
+
+type StackTags = {
+  Environment: Environment;
+};
+
+const $tagResource = (stackTags: StackTags): TagResource => {
+  return function tagResource(resource, resourceTags) {
+    Object.entries({ ...stackTags, ...resourceTags }).forEach(
+      ([key, value]) => {
+        cdk.Tags.of(resource).add(key, value);
+      }
+    );
+  };
+};
 
 const app = new cdk.App();
-new TestCicdCdkStack(app, 'TestCicdCdkStack', {
+
+let env: "prod" | "alpha" | "devel";
+let lambdaRepoName: string;
+try {
+  env = app.node.getContext("env");
+} catch {
+  throw Error(
+    "You must specify a context: devel, alpha, prod. npm run cdk {{COMMAND}} -- --context env={{CONTEXT}}"
+  );
+}
+
+try {
+  lambdaRepoName = app.node.getContext("lambdaRepoName");
+} catch {
+  throw Error("Did not receive lambdaRepoName from context");
+}
+
+const config: StackConfig = {
+  environment: env,
+  namespace: $namespace("cicdk-test", env),
+  tagResource: $tagResource({ Environment: env }),
+  lambdaRepoName: lambdaRepoName,
+};
+
+new TestCicdCdkStack(app, "TestCicdCdkStack", config, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
-
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
-
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/test-cicd-cdk-stack.ts
+++ lib/test-cicd-cdk-stack.ts
@@ -1,16 +1,42 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+export type TagResource = (
+  resource: Construct,
+  resourceTags: { PersistentDataClass: "ephemeral" | "protected" }
+) => void;
+
+export type StackConfig = {
+  environment: string;
+  namespace: (baseName: string) => string;
+  tagResource: TagResource;
+  lambdaRepoName: string;
+};
 
 export class TestCicdCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    config: StackConfig,
+    props?: cdk.StackProps
+  ) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaRepo = cdk.aws_ecr.Repository.fromRepositoryName(
+      this,
+      config.lambdaRepoName,
+      config.lambdaRepoName
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestCicdCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaFunction = new lambda.DockerImageFunction(
+      this,
+      config.namespace("lambda"),
+      {
+        code: lambda.DockerImageCode.fromEcr(lambdaRepo),
+      }
+    );
+
+    config.tagResource(lambdaFunction, { PersistentDataClass: "ephemeral" });
   }
 }
